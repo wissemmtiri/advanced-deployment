adc144e07d1e685188522b267a5645eac3810fa0
unknown <wissem.mtiri@insat.ucar.tn>
2024-04-08 22:25:06 +0100

    configManag = Docker:running

--- Infrastructure Provisioning/version0.tf
+++ Infrastructure Provisioning/version0.tf
@@ -89,8 +89,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
     source_image_reference {
         publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18_04-lts-gen2"
+        offer     = "0001-com-ubuntu-server-focal"
+        sku       = "20_04-lts"
         version   = "latest"
     }
 
